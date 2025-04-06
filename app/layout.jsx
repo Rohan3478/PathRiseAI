ad98df184f3b4111e1da511eb8c80ba26c74042b
Rohan3478 <rohanchauhan8655@gmail.com>
2025-04-06 16:36:00 +0530

    updated footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
             <Toaster richColors/>
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made by Rohan</p>
+                <p>&copy; 2025 PathRiseAI. All Rights Reserved</p>
               </div>
             </footer>
           </ThemeProvider>
